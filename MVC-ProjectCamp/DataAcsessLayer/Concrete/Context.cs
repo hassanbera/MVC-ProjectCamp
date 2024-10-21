9703dd0b8601a5be706f8cfa38dc3892f2475a40
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2024-10-21 20:14:57 +0300

    Context Class Added

--- MVC-ProjectCamp/DataAcsessLayer/Concrete/Context.cs
+++ MVC-ProjectCamp/DataAcsessLayer/Concrete/Context.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAcsessLayer
+namespace DataAcsessLayer.Concrete
 {
-    public class Class1
-    {
-    }
+	public class Context : DbContext
+	{
+		public DbSet<About> 
+	}
 }
